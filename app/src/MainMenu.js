bf439dc2d3c32f72aab6b960dce208b54094cb7e
Laur Sisask <laur.sisask@glia.com>
2020-02-05 12:54:12 +0200

    Add main menu component

---
+++ app/src/MainMenu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MainMenu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Host</h1>
+        <h1>Join</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MainMenu;
