49e004d113d61f5f3e8d53c590c3a5292cdc026f
Aron Sisask <wheezy@wheezy.io>
2020-02-05 15:40:27 +0200

    Add socket.io broadcast server

--- server/app.js
+++ server/app.js
@@ -2,12 +2,37 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const MESSAGE_TYPES = [
+  'playerJoin',
+  'playerList',
+  'gameState',
+  'drawBoard',
+  'countdown'
+];
+
+const broadcastMessage = (messageType, socket) => {
+  return message => {
+    socket.broadcast.emit(messageType, message);
+    console.log(messageType + ':', message);
+  }
+};
+
+const registerMessageHandlers = socket => {
+  for (let i = 0; i < MESSAGE_TYPES.length; i++) {
+    const messageType = MESSAGE_TYPES[i];
+    console.log('Registering message type', messageType);
+    socket.on(messageType, broadcastMessage(messageType, socket));
+  }
+};
+
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-io.on('connection', function(socket){
+io.on('connection', socket => {
   console.log('User has connected');
+
+  registerMessageHandlers(socket);
 });
 
 http.listen(14000, function(){
