438abc3a2cca9a7b2bb05aae05975e3533ab4087
Laur Sisask <laur.sisask@glia.com>
2020-02-05 14:29:07 +0200

    Add basic game states

---
+++ app/src/HostScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HostScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Host screen</h1>;
+  }
+}
+
+export default HostScreen;
