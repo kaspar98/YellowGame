7da1f631d8f9708f024c617ba98981ef1e81498b
VeArnold <56921374+VeArnold@users.noreply.github.com>
2020-02-05 19:20:23 +0200

    guessing screen update

--- app/src/GuessingScreen.js
+++ app/src/GuessingScreen.js
@@ -2,7 +2,24 @@ import React from "react";
 
 class GuessingScreen extends React.Component {
   render() {
-    return <h1>Guess what's being drawn!</h1>;
+    return (
+      <div className="guessing-screen">
+        <h1>What's being drawn?</h1>
+
+        <div className="guessing-screen-keskele">
+          <div className="action-button animate button-text violet">
+            BASKETBALL
+          </div>
+          <div className="action-button animate button-text red">BASEBALL</div>
+        </div>
+        <div className="guessing-screen-keskele">
+          <div className="action-button animate button-text green">
+            FOOTBALL
+          </div>
+          <div className="action-button animate button-text cyan">LIME</div>
+        </div>
+      </div>
+    );
   }
 }
 
